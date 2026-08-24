efd3fab8783a8e808456e90a699e543a3250e1fa
jamisonvancleve <37937928+jamisonvancleve@users.noreply.github.com>
2026-08-24 11:25:44 -0500

    Added get_devices() function to api.py Combined get_devices() and get_users() logic into a single helper function: _fetch_graph_resource and turned get_devices() and get_users() into wrapper functions.

--- app/api.py
+++ app/api.py
@@ -2,15 +2,26 @@
 
 import requests
 import logging
-
 import requests.exceptions
-
 from app.auth import get_graph_token
 
 #Create a logger object for this module
 logger = logging.getLogger(__name__)
 
+#Set base URL for endpoint
+graph_base_url = "https://graph.microsoft.com/v1.0"
+
+
 def get_users():
+    """Fetches user objects from Microsoft Graph API"""
+    return _fetch_graph_resource("users")
+
+def get_devices():
+    """Fetches device objects from Microsoft Graph API"""
+    return _fetch_graph_resource("devices")
+
+def _fetch_graph_resource(resource_type):
+    """Internal helper to run Microsoft Graph API requests with error handling"""
     #Get auth token
     graph_token = get_graph_token()
 
@@ -23,7 +34,10 @@ def get_users():
         "Authorization": f"Bearer {graph_token}",
         "Content-Type": "application/json",
     }
-    endpoint = "https://graph.microsoft.com/v1.0/users"
+    endpoint = f"{graph_base_url}/{resource_type.lstrip('/')}"
+
+    #Debug print
+    #print("endpoint: ",endpoint)
 
     try:
         response = requests.get(endpoint, headers=headers, timeout=10)
@@ -32,16 +46,16 @@ def get_users():
         #Debug print
         #print(response.json())
         return response.json()
+
     except requests.exceptions.Timeout:
         logger.error("Timeout error: request timed out while connecting to Microsoft Graph API.")
         return None
 
     except requests.exceptions.ConnectionError:
-        logger.error("Network connection error. Check Internet connection, DNS, or proxy settings.")
+        logger.error("Network connection error. Check Internet connection, DNS, and proxy settings.")
         return None
 
     except requests.exceptions.HTTPError as err:
-        #status_code = err.response.status_code
         status_code = err.response.status_code if err.response is not None else None
 
         match status_code:
@@ -50,7 +64,6 @@ def get_users():
             case 403:
                 logger.error("403 Forbidden: Ensure proper API permissions have been granted.")
             case 429:
-                # noinspection unresolved-references
                 retry_after = err.response.headers.get("Retry-After", "unknown")
                 logger.error(f"429 Throttled: Microsoft Graph requested wait time of {retry_after} seconds.")
             case _:
@@ -59,7 +72,7 @@ def get_users():
         return None
 
     except requests.exceptions.JSONDecodeError:
-        logger.error("Error decoding JSON. Response is not formatted correctly.")
+        logger.error("Error decoding JSON. Response is not properly formatted.")
         return None
 
     except requests.exceptions.RequestException as err:
@@ -67,19 +80,18 @@ def get_users():
         logger.error(f"Unexpceted error: {err}")
         return None
 
-
-
-
-
 if __name__ == "__main__":
+    #Testing both endpoints
+
     #Call get_users(), then look inside the dictionary that was returned for the key named 'value'.
     #If found, assign its contents to the users variable. If missing, set users to an empty list '[]'
-    # noinspection unresolved-references
     users = get_users().get("value",[])
 
     #The next two lines accomplish the same as the single line above. Less efficient, but more readable.
     #data = get_users()
     #users = data.get("value", [])
+
+    #Print each user in the list and the total number below
 
     for user in users:
         name = user.get("displayName")
@@ -89,3 +101,22 @@ if __name__ == "__main__":
         print(f"Name: {name}, Title: {title}, upn: {upn}")
 
     print(f"\nTotal users returned: {len(users)}")
+
+
+
+
+    #Call devices(), then look inside the dictionary that was returned for the key named 'value'.
+    #If found, assign its contents to the devices variable. If missing, set devices to an empty list '[]'
+    devices = get_devices().get("value",[])
+
+    #Print each device in the list and the total number below
+
+    for device in devices:
+        name = device.get("displayName")
+        deviceId = device.get("deviceId")
+        os = device.get("operatingSystem")
+        os_version = device.get("operatingSystemVersion")
+
+        print(f"Name: {name}, deviceId: {deviceId}, os: {os}, os_version: {os_version}")
+
+    #print(f"\nTotal users returned: {len(users)}")
